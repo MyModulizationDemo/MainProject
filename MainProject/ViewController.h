a1d44ea9780c716a6db5abf0e3ee07974cebda0c
J-JasonZz <zzujasonzhang@gmail.com>
2017-04-19 15:53:18 +0800

    0.0.1

---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by JasonZhang on 2017/4/19.
+//  Copyright © 2017年 JasonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MainProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MainProject
+//
+//  Created by JasonZhang on 2017/4/19.
+//  Copyright © 2017年 JasonZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
